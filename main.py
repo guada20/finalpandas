816b349896401d1b2d66cf099e842c7b97eef791
207 <207@academicos.cesde.edu.co>
2023-07-18 17:30:41 -0500

    ok

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+from data.platos import platosPopulares
+from data.creartabla import crearTabla
+
+tablaPlatos=pd.DataFrame(platosPopulares)
+print(tablaPlatos)
+crearTabla(tablaPlatos,"tablaplatospopulares")
